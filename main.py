4b911446ad008c1ad7f70755967e0fc34af03f83
1d-sulecki <@>
2021-12-08 08:54:23 +0100

    1

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-o = x = []
-for i in range(10):
-    tem = input("?")
-    x.append()
+#o = x = ([1,2],(1,2))
+#for i in range(10):
+    #tem = input("?")
+#x.append('1')
+x = ["-","-","-"]
+y = ["-","-","-"]
+z = ["-","-","-"]
+
+
+print('\n',x,'\n',y,'\n',z)
