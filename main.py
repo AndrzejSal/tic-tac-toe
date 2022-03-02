2f880b84ae8c45515432f5809032939babc95aee
1d-sulecki <@>
2022-03-02 09:21:41 +0100

    czyszczenie

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os#o = x = ([1,2],(1,2))
-#for i in range(10):
-    #tem = input("?")
-#x.append('1')
+
 x = ["-","-","-"]
 y = ["-","-","-"]
 z = ["-","-","-"]
@@ -10,8 +7,6 @@ def plansza():
     #print('\n','\n','\n','\n','\n','\n','\n','\n')
 #    #os.system("clear")
     print('\n',*x,'\n',*y,'\n',*z,sep=" ") #,end="").
-#plansza()
-#k = print ("Podaj koordynaty pola")
 def ustaw(co,gdzie):
     gdzie -= 1
     if gdzie > 5:
@@ -23,16 +18,11 @@ def ustaw(co,gdzie):
     else:
         z[gdzie] = co
         #print(z)
-#print("",end="")
-#print(y)
 plansza()
 
 def przeczytaj(gdzie):
     global x,y,z
     g = {1:z,2:z,3:z,4:y,5:y,6:y,7:x,8:x,9:x,}
-    #print("[A]",gdzie)
-  #  print("[B]",g[gdzie])
-   # print("D",str(g[gdzie]))
     if gdzie < 4:
         return (g[int(gdzie)])[int(gdzie-1)]
     elif gdzie < 7:
@@ -40,27 +30,7 @@ def przeczytaj(gdzie):
     elif gdzie < 10:
         return (g[int(gdzie)])[int(gdzie-7)]
     else:
-        return ('\033[91m'"Z")
-    #if [7,8,9].count(gdzie) == 1:
-     #   if gdzie == 7:
-      #      return(x[0])
-       # elif gdzie == 8:
-        #    return
-    #elif [4,5,6].count(gdzie) == 1:
-    #elif [1, 2, 3].count(gdzie) == 1:
-#for i in range(10):
- #   print(przeczytaj(i+1))
-#for i in range(1,10):
-    #print("i {}".format(i))
- #   ustaw("X",i)
-  #  plansza()
-#ustaw("XDDDDDDD",4)
-#plansza()
-
-
-
-
-
+        return ("Błąd adresu")
 
 
 
