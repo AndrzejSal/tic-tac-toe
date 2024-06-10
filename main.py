0313421a8094bb596614194be7297defadb29d11
AndrzejSal <93601266+AndrzejSal@users.noreply.github.com>
2024-06-10 09:11:30 +0200

    Update main.py
    optymalizacja dodwania

--- main.py
+++ main.py
@@ -8,16 +8,8 @@ def plansza():
 #    #os.system("clear")
     print('\n',*x,'\n',*y,'\n',*z,sep=" ") #,end="").
 def ustaw(co,gdzie):
-    gdzie -= 1
-    if gdzie > 5:
-        gdzie-= 6
-        x[gdzie] = co
-    elif gdzie > 2:
-        gdzie -=3
-        y[gdzie] = co
-    else:
-        z[gdzie] = co
-        #print(z)
+    g = [z,y,x][(gdzie-1)//3]
+    g[gdzie-1)%3] = co
 plansza()
 
 def przeczytaj(gdzie):
@@ -26,8 +18,19 @@ def przeczytaj(gdzie):
     return g[(gdzie-1)%3]
     #else:
     #    return ("Błąd adresu")
-
-
+def check():
+    for i in range(3):
+        if przeczytaj(3*i)==przeczytaj(3*i+1)==przeczytaj(3*i+2)!="-":
+            return przeczytaj(3*i)
+        if przeczytaj(i)==przeczytaj(i+3)==przeczytaj(i+6)!="-":
+            return przeczytaj(i)
+    for i in range(2):
+        if przeczytaj(i)==przeczytaj(1-i)==przeczytaj(2-i)!="-":
+            return przeczytaj(i)
+    return "-"
+    
+        
+        
 
 
 
